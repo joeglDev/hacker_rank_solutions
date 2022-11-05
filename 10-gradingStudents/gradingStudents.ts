43db1c87ac3f4ba7ba3e2c768549614c84cc9753
Hiroji <joegilbertdev@outlook.com>
2022-11-05 15:13:18 +0000

    reafactor

--- 10-gradingStudents/gradingStudents.ts
+++ 10-gradingStudents/gradingStudents.ts
@@ -3,31 +3,35 @@
 
 <38 auto fail
 
-If the difference between the and the next multiple of is less than , round up to the next multiple of .
+If the difference between the and the next multiple of is less than 3, round up to the next multiple of .
 */
 
 function gradingStudents(grades: number[]): number[] {
   const gradesCopy = [...grades];
-  gradesCopy.forEach((grade) => {
-    if (grade < 38) {
-      gradesCopy.splice(gradesCopy.indexOf(grade), 1);
+  console.log(gradesCopy);
+
+  gradesCopy.forEach((grade, index) => {
+    console.log("current", gradesCopy[index])
+    console.log(gradesCopy)
+    //remove grades < 38
+    if (gradesCopy[index] < 38) {
+      console.log("removing grade", grade);
+      gradesCopy.splice(index, 1);
+      
     } else {
-      for (let i = 1; i < 106; i++) {
-        let count = grade + 1;
-        if (count % 5 === 0) {
-          let newGrade = count;
-          if (newGrade - grade < 3) {
-            gradesCopy.splice(gradesCopy.indexOf(grade), 1, newGrade);
-          }
+
+      for (let i = 1; i < 101; i++) {
+        let newGrade = i + gradesCopy[index ]
+        console.log(grade, newGrade)
+        if (newGrade % 5 === 0 && (newGrade - gradesCopy[index ] < 3)) {
+          console.log("divides by 5", newGrade);
+          gradesCopy.splice(index, 1, newGrade);
+          break;
         }
       }
     }
-    //get next multiple of 5
-    // add onto grade
-    //check if diff less than 3 if so return in place
   });
-
   return gradesCopy;
 }
 
-console.log(gradingStudents([4, 73, 67, 38, 33])); //75 67 40 33
+console.log(gradingStudents([4, 73, 67, 38, 33])); //4, 73, 67, 38, 33 => 75 67 40 33
