7400ea5e102d94133fe165daf53548a5dd76866c
Hiroji <joegilbertdev@outlook.com>
2022-11-12 09:57:40 +0000

    wiped 10

--- 10-gradingStudents/gradingStudents.ts
+++ 10-gradingStudents/gradingStudents.ts
@@ -7,31 +7,9 @@ If the difference between the and the next multiple of is less than 3, round up 
 */
 
 function gradingStudents(grades: number[]): number[] {
-  const gradesCopy = [...grades];
-  console.log(gradesCopy);
+  const newGrades: number[] = [];
 
-  gradesCopy.forEach((grade, index) => {
-    console.log("current", gradesCopy[index])
-    console.log(gradesCopy)
-    //remove grades < 38
-    if (gradesCopy[index] < 38) {
-      console.log("removing grade", grade);
-      gradesCopy.splice(index, 1);
-      
-    } else {
-
-      for (let i = 1; i < 101; i++) {
-        let newGrade = i + gradesCopy[index ]
-        console.log(grade, newGrade)
-        if (newGrade % 5 === 0 && (newGrade - gradesCopy[index ] < 3)) {
-          console.log("divides by 5", newGrade);
-          gradesCopy.splice(index, 1, newGrade);
-          break;
-        }
-      }
-    }
-  });
-  return gradesCopy;
+  return newGrades;
 }
 
 console.log(gradingStudents([4, 73, 67, 38, 33])); //4, 73, 67, 38, 33 => 75 67 40 33
