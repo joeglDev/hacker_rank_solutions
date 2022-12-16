72156ac93a4af533141290e894abf37443042669
Hiroji <joegilbertdev@outlook.com>
2022-12-16 16:56:47 +0000

    15 pangrams- passes tests

--- 15-pangrams/pangrams.ts
+++ 15-pangrams/pangrams.ts
@@ -1,7 +1,23 @@
+function pangrams(s: string): string {
+  const alphabet: any = {};
+  const splitInput = s.split("");
 
-export function pangrams(s: string): string {
-    return 'pangram'
-    // add to hashmap if not found in 
-    //fails ih hashmap doesnt have 27 keys
-
+  //for each character 
+  //checks is a letter
+  //checks if present in alphabet hashmap if not adds as a new key
+  splitInput.forEach((char) => {
+    const capitalChar = char.toUpperCase();
+    if (/[A-Z]/.test(capitalChar)) {
+      if (!alphabet[capitalChar]) {
+        alphabet[capitalChar] = true;
+      }
+    }
+  });
+  //outside of loop checks that alphabet length is present
+  if (Object.keys(alphabet).length === 26) {
+    return "pangram";
+  }
+  return "not pangram";
 }
+
+export default pangrams;
