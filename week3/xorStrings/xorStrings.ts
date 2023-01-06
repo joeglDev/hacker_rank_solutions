10bc8bc4d13f44e5da92e5aa2219b3c5b5a8a47f
Hiroji <joegilbertdev@outlook.com>
2023-01-06 14:52:19 +0000

    week3/xorStrings: solution passes tests for strings

--- week3/xorStrings/xorStrings.ts
+++ week3/xorStrings/xorStrings.ts
@@ -1,5 +1,12 @@
-function main() {
-    // Enter your code here
-};
+// true if and only if its arguments differ
 
-export default main;
+function xorStrings(A: string, B: string): string {
+  let xor = "";
+  for (let i = 0; i < A.length; i++) {
+    A[i] !== B[i] ? (xor += "1") : (xor += "0");
+  }
+
+  return xor;
+}
+
+export default xorStrings;
