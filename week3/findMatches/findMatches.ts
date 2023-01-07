47436be904ab4dee65392f50d655bdaec9979fe4
Hiroji <joegilbertdev@outlook.com>
2023-01-07 14:30:26 +0000

    week3/findMatches- added documentation

--- week3/findMatches/findMatches.ts
+++ week3/findMatches/findMatches.ts
@@ -1,8 +1,16 @@
+
+/**
+ * Returns number of elements of an input array which form a matching pair.
+ *
+ *  @param {Array<number>} ar - Input array of numbers
+ * @return {number} count Number of matching pairs found.
+ */
 function findMatches(ar: number[]): number {
   // get unique elements
   let uniqueArray = [...new Set(ar)];
   let count = 0;
   uniqueArray.forEach((element) => {
+    //checks that pairs of matches are found
     let numberOf = 0;
     ar.forEach((number) => {
       if (number === element) {
@@ -14,24 +22,6 @@ function findMatches(ar: number[]): number {
     });
   });
   return count;
-  /*
-  //loop through array if match to index then add to holding arr and remove from arr
-  //count subarrays in holding arr
-  const inputArr = [...ar];
-  const arLength = ar.length;
-  let numberOfMatches = 0;
-  for (let i = 0; i < arLength; i++) {
-    let checkTwo = 0;
-    for (let k = i + 1; k < arLength; k++) {
-      if (inputArr[i] === inputArr[k]) {
-        checkTwo += 1;
-        if (checkTwo === 2) {numberOfMatches += 1;}
-        
-      }
-    }
-  }
-  return numberOfMatches;
-  */
 }
 
 export { findMatches };
