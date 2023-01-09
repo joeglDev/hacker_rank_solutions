1af6b06f499e4486d5fa623cb04267aa6ad1b981
Hiroji <joegilbertdev@outlook.com>
2023-01-09 13:28:01 +0000

    week3/findMode: init

---
+++ week3/findMode.ts
@@ -0,0 +1,18 @@
+// solution for hacker rank migratoryBirds problem see https://www.hackerrank.com/challenges/three-month-preparation-kit-migratory-birds
+
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function findMode(arr: number[]): number {
+    // Write your code here
+    let mode = 0;
+    return mode;
+
+};
+
+export {findMode};
+
