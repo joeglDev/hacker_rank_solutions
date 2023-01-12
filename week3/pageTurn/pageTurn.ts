6955109b6e63328ea84d039dc347cfcb34104d66
Hiroji <joegilbertdev@outlook.com>
2023-01-12 18:48:51 +0000

    week3/pageCount: init

---
+++ week3/pageTurn/pageTurn.ts
@@ -0,0 +1,15 @@
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+export function pageCount(n: number, p: number): number   {
+    // Write your code here
+    return 1;
+
+};
+
