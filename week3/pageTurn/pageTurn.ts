4ce7ca3a317feaec03af9069bc8aee3834ddb926
Hiroji <joegilbertdev@outlook.com>
2023-01-12 20:22:42 +0000

    week3/pageTurn: added jsDoc

--- week3/pageTurn/pageTurn.ts
+++ week3/pageTurn/pageTurn.ts
@@ -1,5 +1,6 @@
 /*
  * Complete the 'pageCount' function below.
+* Given n = book length in pages and p page to find, find and print the minimum number of pages that must be turned in order to arrive at page . 
  *
  * The function is expected to return an INTEGER.
  * The function accepts following parameters:
@@ -7,6 +8,14 @@
  *  2. INTEGER p
  */
 
+
+/**
+ * Function calculates the minimum number of page turns required to reavh a given page p of a book of pages p. 
+ * 
+ * @param {number} n - Length of the book in pages.
+ * @param {number} p - Page number of the page to reach.
+ * @returns {number} pagesToTurn - Number of page turns required to reach page p.
+ */
 export function pageCount(n: number, p: number): number   {
     // Write your code here
     return 1;
