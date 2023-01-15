c4f59e32324bca83c9ae3f7b7a80ee6d30468e49
Hiroji <joegilbertdev@outlook.com>
2023-01-15 19:09:39 +0000

    zigzag: init

---
+++ week3/zigzag/zigzag.ts
@@ -0,0 +1,10 @@
+/**
+ * Returns the input array arr permuted into a zigzag array. A array where numbers increase then decrease in size after index k by k = (n + 1) / 2. n = length of array.
+ *
+ * @param {Array.<number>} arr  - Input array of numbers.
+ * @returns {Array.<number>} - Returns permuted array of numbers representing the zigzag array.
+ */
+export const zigzag = (arr: number[]): number[] => {
+    const indexToDecreaseAfter = (arr.length + 1) / 2;
+  return [1];
+};
