143582389a8175ead6b2dd90b669f9432b5db6bc
Hiroji <joegilbertdev@outlook.com>
2023-01-15 19:37:53 +0000

    zigzag: partial solution

--- week3/zigzag/zigzag.ts
+++ week3/zigzag/zigzag.ts
@@ -1,10 +1,26 @@
 /**
- * Returns the input array arr permuted into a zigzag array. A array where numbers increase then decrease in size after index k by k = (n + 1) / 2. n = length of array.
+ * Returns the input array arr permuted into a zigzag array.
+ *  An array where numbers increase then decrease in size after index k by k = (n + 1) / 2. n = length of array.
  *
  * @param {Array.<number>} arr  - Input array of numbers.
  * @returns {Array.<number>} - Returns permuted array of numbers representing the zigzag array.
  */
 export const zigzag = (arr: number[]): number[] => {
-    const indexToDecreaseAfter = (arr.length + 1) / 2;
-  return [1];
+    //indexToDecreaseAfter 
+    const k = (arr.length + 1) / 2;
+    let lengthCount = arr.length;
+    //sort by asc
+    //select relevant number from start and move to end
+
+    const sortedArr = arr.sort((a, b) => {return a - b});
+    
+  //pick first number and move to end until arr.length - k = 1
+    while (lengthCount - k >= 1) {
+        const selectedElement = sortedArr[0];
+        sortedArr.splice(0, 1);
+        sortedArr.push(selectedElement);
+        lengthCount -= 1;
+    }
+
+  return sortedArr;
 };
