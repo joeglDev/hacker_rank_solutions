625f8347bcdcc9b3fc533a1dffd8a567dd23b4b3
Hiroji <joegilbertdev@outlook.com>
2023-01-20 20:22:33 +0000

    maxPerimeterTRiangle: setup

---
+++ week3/maxPerimeterTriangle/maxPerimeterTriangle.ts
@@ -0,0 +1,15 @@
+
+/**
+ * Calculates the maximum perimeter of a non-degenerate triangle from an input array of numbers.
+ * 
+ * @param {Array.<number>} sticks -  Array of numbers representing sides of a possible triangle.
+ * @returns {Array.<number>} - Array of numbers representing the perimeter of a triangle.
+ */
+export const maxPerimeterTriangle = (sticks: number[]): number[] => {
+    //return sum of sides length as arr in  on-decreasing order
+    //else 
+    //Choose the one with the longest maximum side. 
+    //If more than one has that maximum, choose from them the one with the longest minimum side. 
+    //If more than one has that maximum as well, print any one them.
+    return [-1]
+};
