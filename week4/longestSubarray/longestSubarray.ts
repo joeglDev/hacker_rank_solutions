feb790fb46171ef661ce819bbbd7d362d00d206a
Hiroji <joegilbertdev@outlook.com>
2023-02-04 13:35:59 +0000

    week4/longestSubArray: broken code

--- week4/longestSubarray/longestSubarray.ts
+++ week4/longestSubarray/longestSubarray.ts
@@ -5,5 +5,31 @@
  * @returns {number}  - length of the largest subarray found.
  */
 export function pickingNumbers(a: number[]): number {
-  return 0;
+  //loop through each element of the array
+  //assign element to var
+
+  //loop though +1 and assign to sub array if meets condition
+
+  //vars
+    const allSubArrays: number[][] = [];
+
+  //logic
+  a.forEach((element, index) => {
+    const subArray = [];
+    subArray.push(a[index]);
+    for (let i = index; i < a.length; i++) {
+      if (Math.abs(element - a[i]) <= 1) {
+        subArray.push(a[i]);
+      }
+    }
+    allSubArrays.push(subArray);
+  });
+
+  //loop through all subarrays and get largest
+  let maxLength = 0;
+  allSubArrays.forEach((arr) => {
+    if (arr.length > maxLength) {maxLength = arr.length}
+  })
+  console.log(maxLength)
+  return maxLength;
 }
