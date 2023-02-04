bf93ecc53ccf34607dfcec9457aaa2a60134e633
Hiroji <joegilbertdev@outlook.com>
2023-02-04 13:09:25 +0000

    week4/longestSubarray: setup

---
+++ week4/longestSubarray/longestSubarray.ts
@@ -0,0 +1,9 @@
+/**
+ * Given an array of integers, returns the longest subarray where the absolute difference between any two elements is less than or equal to 1.
+ *
+ * @param {number[]} a - input array to test.
+ * @returns {number}  - length of the largest subarray found.
+ */
+export function pickingNumbers(a: number[]): number {
+  return 0;
+}
