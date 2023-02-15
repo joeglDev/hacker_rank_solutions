f9b00cba30beb45e53eb291fc6deff1b3f812bcd
Hiroji <joegilbertdev@outlook.com>
2023-02-15 11:24:38 +0000

    longestSubarray: correct solution

--- week4/longestSubarray/longestSubarray.ts
+++ week4/longestSubarray/longestSubarray.ts
@@ -5,46 +5,17 @@
  * @returns {number} maxLength - length of the largest subarray found.
  */
 export function pickingNumbers(a: number[]): number {
-  //loop through each element of the array
-  //assign element to var
+  a.sort((l, m) => m - l);
+  let max = 0;
 
-  //loop though +1 and assign to sub array if meets condition
-
-  //vars
-  const allSubArrays: number[][] = [];
-
-  //logic
-  a.forEach((element, index) => {
-    const subArray = [];
-    subArray.push(a[index]);
-    for (let i = index + 1; i < a.length; i++) {
-      if (Math.abs(element - a[i]) <= 1) {
-        subArray.push(a[i]);
-      }
+  for (let i = 0; i < a.length - 1; i++) {
+    let count = 0;
+    for (let j = i; j < a.length; j++) {
+      if (a[i] - a[j] <= 1) {
+        count++;
+        if (count > max) {max = count};
+      } else count = 0;
     }
-    allSubArrays.push(subArray);
-  });
-
-  //check each arr in subarrays and if diff between elements is greater than 1 exclide
-  allSubArrays.forEach((arr, arrIndex) => {
-    let minValue = Math.min(...arr);
-    let maxValue = Math.max(...arr);
-    arr.forEach((element, elementIndex) => {
-      if (
-        Math.abs(minValue - element) > 1 ||
-        Math.abs(maxValue - element) > 1
-      ) {
-        arr.splice(elementIndex + 1, 1);
-      }
-    });
-  });
-  console.log(allSubArrays);
-  let maxLength = 0;
-  allSubArrays.forEach((arr) => {
-    //loop through all subarrays and get largest
-    if (arr.length > maxLength) {
-      maxLength = arr.length;
-    }
-  });
-  return maxLength;
+  }
+  return max;
 }
