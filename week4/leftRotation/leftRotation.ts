3cac8adf5ae35fa4a1a2672d1a92f4791b739428
Hiroji <joegilbertdev@outlook.com>
2023-02-17 14:15:39 +0000

    leftRotation: passed first test

--- week4/leftRotation/leftRotation.ts
+++ week4/leftRotation/leftRotation.ts
@@ -1,18 +1,22 @@
-/** 
+/**
  * Rotates numbers in an array by d places to the left and returns the new array.
- * 
+ *
  * @param {number} d - THe number of places to shift elements of the array left by.
  * @param {number[]} arr - Input array of numbers to left shift.
- * 
+ *
  * @returns {number[]} - Returned array of numbers shifted left by d places.
- * 
+ *
  **/
 
 export function rotateLeft(d: number, arr: number[]): number[] {
-    // shift array d numbers to left
+  // 1 - grab d first elements and save
+  // 2 - grab remaining elements and save
+  // 3 new arr and return
 
-    // 1 - grab d first elements and save
-    // 2 - grab remaining elements and save 
-    // 3 new arr and return
-return []
+  const firstElements = arr.slice(0, d);
+  const lastElements = arr.slice(d, arr.length);
+  console.log(firstElements, lastElements);
+  const leftShiftedArr = firstElements.concat(lastElements);
+
+  return leftShiftedArr;
 }
