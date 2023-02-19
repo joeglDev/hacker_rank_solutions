546d486f9caeafd73b1d01afd8a49941b486e55f
Hiroji <joegilbertdev@outlook.com>
2023-02-19 14:53:24 +0000

    numberLineJumps: refactor

--- week4/numberLineJumps/numberLineJumps.ts
+++ week4/numberLineJumps/numberLineJumps.ts
@@ -17,23 +17,18 @@ export function kangaroo(
   if (x1 > x2 && v1 > v2) {
     return "NO";
   } else if (x1 > x2 && v2 > v1) {
-    if ((x1 % v2) === 0) {
+    if (x1 % v2 === 0) {
       return "YES";
     } else {
       return "NO";
     }
   } else if (x2 > x1 && v1 > v2) {
-    if ((x2 % v1) === 0) {
-        return "YES";
-      } else {
-        return "NO";
-      }
-  }
-   else {
+    if (x2 % v1 === 0) {
+      return "YES";
+    } else {
+      return "NO";
+    }
+  } else {
     return "NO";
   }
-
-  //x2 > x1 && v1 > v2
-  //x2 > x1 && v2 > v1
-  return "";
 }
