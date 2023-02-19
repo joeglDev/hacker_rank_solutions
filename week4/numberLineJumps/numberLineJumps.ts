4fc4d6886486ee18a6a8283d6430653d0ce1d8a2
Hiroji <joegilbertdev@outlook.com>
2023-02-19 14:21:15 +0000

    numberLineJumps: init

---
+++ week4/numberLineJumps/numberLineJumps.ts
@@ -0,0 +1,18 @@
+/**
+ * Models the movement of two objects along a arbitary positive line in steps per turn. 
+ * Returns "YES" if two objects meet and "NO" if not.
+ *
+ * @param {number}  x1 - PLacement of object 1.
+ * @param {number}  v1 - Velocity per turn of object 1.
+ * @param {number}  x2 - Placement of object 2.
+ * @param {number}  v2 - Velocity of object 2.
+ * @returns {string} - Returns "YES" if two objects meet and "NO" if not.
+ */
+export function kangaroo(
+  x1: number,
+  v1: number,
+  x2: number,
+  v2: number
+): string {
+  return "";
+}
